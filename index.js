b85c988ea49427bd3d8aa80ede3d2d3dcb6f5e67
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-10-15 11:08:50 +0200

    feat: progress bar style working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // *****VARS*****
 let password = '';
+// *****VARS*****
 
 // *****DOM ELEMENTS*****
 // Input
@@ -10,103 +11,112 @@ const capitalRule = document.getElementById('capital-rule');
 const numberRule = document.getElementById('number-rule');
 const specialRule = document.getElementById('special-rule');
 // Progress bar
-const progressBar = document.getElementById('Bar')
-// Counter for conditions meeting
-let conditionCount = 0;
+const progressBar = document.getElementById('Bar');
+// *****DOM ELEMENTS*****
 
 
 // *****FUNCTIONS*****
-
 // HELPERS
-// Capital rule
+// Capitals check
 function containsCapitalLetter(str) {
     const capitalChars = /[A-Z]/;
     return capitalChars.test(str);
 }
 
-// Number rule
+// Numbers check
 function containsNumbers(str) {
     const numberChars = /[0-9]/;
     return numberChars.test(str);
 }
 
-// Special char rule
+// Special chars check
 function containsSpecialChars(str) {
-    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    const specialChars = /[`!@#$%^&*()_+\-=$begin:math:display$$end:math:display${};':"\\|,.<>\/?~]/;
     return specialChars.test(str);
 }
-// HELPERS
 
 // UPDATE PROGRESS BAR
-function updateProgressBar() {
-    // Multiply 25 by the time each condition is met
+function updateProgressBar(conditionCount) {
+    // Multiply 25 by the number of conditions met
     const widthPercentage = conditionCount * 25;
-
-    // Set the width of the bar
+    // Apply to width
     progressBar.style.width = `${widthPercentage}%`;
-    
-    // Set style-color conditionally
-    if (progressBar.style.width === '25%') {
+
+    // Update bar based on percentage
+    // Remove every class may be applied
+    progressBar.classList.remove('bg-danger', 'bg-warning', 'bg-info', 'bg-success');
+    // If a condition is met apply a certain class
+    if (widthPercentage === 25) {
         progressBar.classList.add('bg-danger');
-        progressBar.innerHTML = '25%';
-    } else if (progressBar.style.width === '50%') {
-        progressBar.classList.remove('bg-danger');
+    } else if (widthPercentage === 50) {
         progressBar.classList.add('bg-warning');
-        progressBar.innerHTML = '50%';
-    } else if (progressBar.style.width === '75%') {
-        progressBar.classList.remove('bg-warning');
+    } else if (widthPercentage === 75) {
         progressBar.classList.add('bg-info');
-        progressBar.innerHTML = '75%';
-    } else if (progressBar.style.width === '100%') {
-        progressBar.classList.remove('bg-info');
+    } else if (widthPercentage === 100) {
         progressBar.classList.add('bg-success');
-        progressBar.innerHTML = '100%';
     }
+    // Type the percentage as text
+    progressBar.innerHTML = `${widthPercentage}%`;
 }
 
 // CHECK PASSWORD
-function checkPassword (str) {
+function checkPassword() {
+    // Count the number of condition met
+    // This reset keeps the bar always updated because each time i add or remove chars it reset to 0
+    // rechecks (so every condition it's checked) and count how many conditions are met, then update the bar conditionally based on the function that to that
+    let conditionCount = 0;
+
     // PASS LENGTH RULE
     if (password.length > 8 ) {
+        // Increase condition met
+        conditionCount++;
+        // Text style
         charRule.innerHTML = '<i class="fa-solid fa-check"></i> At least 9 characters';
         charRule.classList.add('text-success');
     } else {
+        // Removes style if for exemple i delete chars and the condition it's no longer met
         charRule.innerHTML = '<i class="fa-solid fa-x"></i> At least 9 characters';
         charRule.classList.remove('text-success');
     }
 
-    // Capital rule applied
+    // CAPITAL RULE
     if (containsCapitalLetter(password)){
+        conditionCount++;
         capitalRule.innerHTML = '<i class="fa-solid fa-check"></i> At least 1 capital letter';
         capitalRule.classList.add('text-success');
     } else {
-        capitalRule.innerHTML = '<i class="fa-solid fa-x"></i> At least 1 capital letter'
+        capitalRule.innerHTML = '<i class="fa-solid fa-x"></i> At least 1 capital letter';
         capitalRule.classList.remove('text-success');
     }
 
-
-    // Number rule applied
+    // NUMBER RULE
     if (containsNumbers(password)) {
+        conditionCount++;
         numberRule.innerHTML = '<i class="fa-solid fa-check"></i> At least 1 number';
         numberRule.classList.add('text-success');
     } else {
-        numberRule.innerHTML = '<i class="fa-solid fa-x"></i> At least 1 number'
+        numberRule.innerHTML = '<i class="fa-solid fa-x"></i> At least 1 number';
         numberRule.classList.remove('text-success');
     }
 
-    // Special char applied
+    // SPECIAL CHAR RULE
     if (containsSpecialChars(password)) {
+        conditionCount++;
         specialRule.innerHTML = '<i class="fa-solid fa-check"></i> At least 1 special character';
         specialRule.classList.add('text-success');
     } else {
-        specialRule.innerHTML = '<i class="fa-solid fa-x"></i> At least 1 special character'
+        specialRule.innerHTML = '<i class="fa-solid fa-x"></i> At least 1 special character';
         specialRule.classList.remove('text-success');
     }
-}
 
-//*****EVENT LISTENERS*****
-input.addEventListener('keyup', (e) => {
-    // Save password
-    password = input.value
-    checkPassword(password)
-})
+    // Update the progress bar based on number of conditions met
+    updateProgressBar(conditionCount);
+}
+// *****FUNCTIONS*****
+
+// *****EVENT LISTENERS*****
+input.addEventListener('keyup', () => {
+    password = input.value;
+    checkPassword();
+});
+// *****EVENT LISTENERS*****
